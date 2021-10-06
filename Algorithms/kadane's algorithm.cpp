a5881c39a266c6e78e6f88982516a7de45e02fa4
coder284 <devanshagarwal100@gmail.com>
2021-10-06 15:13:11 +0530

    added kadane's algorithm

---
+++ Algorithms/kadane's algorithm.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+int main(){
+int n;
+cin>>n;
+//assuming that empty sub-arrays are allowed as then the sum can be zero
+int a[100]={0};
+for(int i=0;i<n;i++){
+    cin>>a[i];
+}
+int max_sum=0;
+int current_sum=0;
+for(int i=0;i<n;i++){
+    current_sum+=a[i];
+    if(current_sum<0){
+        current_sum=0;
+    }
+    max_sum=max(max_sum,current_sum);
+}
+cout<<max_sum<<endl;
+return 0;
+}
